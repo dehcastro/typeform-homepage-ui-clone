95e83f48ec6d6766c6936f9540cc8cf89accea68
André de Castro <andre_vc@hotmail.com>
2020-09-16 17:20:25 -0300

    :zap: chore: finished base structure

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+export const Container = styled(motion.header)`
+    width: 100%;
+    height: 200px;
+    background: rgba(0 , 0, 0, 0.4);
+
+    position: fixed;
+    z-index: 2;
+`;
